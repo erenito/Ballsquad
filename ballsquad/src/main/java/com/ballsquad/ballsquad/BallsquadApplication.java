d7cf85ad83fb668cd9a5c2e62cc70a7a7ecc6b41
Eren Akgun <yeren@sabanciuniv.edu>
2024-04-01 16:12:03 +0200

    Spring Boot App initialized.

---
+++ ballsquad/src/main/java/com/ballsquad/ballsquad/BallsquadApplication.java
@@ -0,0 +1,13 @@
+package com.ballsquad.ballsquad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BallsquadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BallsquadApplication.class, args);
+	}
+
+}
---
+++ ballsquad/src/test/java/com/ballsquad/ballsquad/BallsquadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ballsquad.ballsquad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallsquadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
